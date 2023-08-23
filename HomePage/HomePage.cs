856b2f7a52f3f3ffc9ca4993e8f34ce2b377b768
Kim Bandeleon <kbandeleon@gmail.com>
2023-08-23 21:53:35 +0800

    Add test SearchCityAndValidateInfo

--- HomePage/HomePage.cs
+++ HomePage/HomePage.cs
@@ -12,12 +12,23 @@ namespace Tests
         }
 
         public void Login(string username, string password) {
+            NavigateTo(HomePageVariables.LOGIN_URL);
+
             _driver.FindElement(HomePageVariables.textBoxUsername).SendKeys(username);
             _driver.FindElement(HomePageVariables.textBoxPassword).SendKeys(password);
 
             _driver.FindElement(HomePageVariables.buttonSubmit).Click();
 
-            Thread.Sleep(5000);
+            Thread.Sleep(HomePageVariables.GlobalSleep);
+        }
+        public string GetApiKey()
+        {
+            _driver.FindElement(HomePageVariables.dropdownUserMenu).Click();
+            _driver.FindElement(HomePageVariables.submenuMyApikeys).Click();
+            
+            Thread.Sleep(HomePageVariables.GlobalSleep);
+
+            return _driver.FindElement(HomePageVariables.textApiKeys).Text;
         }
   
         public void Quit() {
